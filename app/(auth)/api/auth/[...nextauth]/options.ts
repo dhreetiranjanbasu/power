37ad0d80b060cf808ac3073adf506213f7897e7c
Dhreeti Ranjan Basu <dhreetiranjanbasu@Dhreetis-MacBook-Pro.local>
2023-12-21 21:12:19 +0600

    new file:   app/(auth)/api/auth/[...nextauth]/options.ts 	modified:   app/actions/getCurrentUser.ts 	modified:   app/layout.tsx

---
+++ app/(auth)/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,23 @@
+
+import NextAuth, { NextAuthOptions } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import prisma from "@/lib/prismadb";
+
+
+
+export const authOptions: NextAuthOptions = ({
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+  ],
+  
+  secret: process.env.NEXTAUTH_SECRET
+});
+
+const handler = NextAuth(authOptions);
+
+export {handler as GET, handler as POST};
